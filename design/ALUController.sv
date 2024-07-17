7cef832886e7708d7488cdebbb770087e7fd4587
Vinicius Lima Sa de Melo <vlsm@windows.cin.ufpe.br>
2024-07-17 16:31:24 -0300

    Jujao ajeitou

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -37,6 +37,5 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)); // SRAI
 
   assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-<
-      ((ALUOp == 2'b10) && (Funct3 == 3'b000)); // ADDI (To em dúvida)
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
 endmodule
